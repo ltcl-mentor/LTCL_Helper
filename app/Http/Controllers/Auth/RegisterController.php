983b5eb25c2f036e20a72431f37705d3144c1137
ltcl-mentor <tomoki.koyama.levtechcollege@gmail.com>
2021-06-01 17:02:19 +0900

    registerを一時表示

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,6 +63,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        dd(User::all());
         return User::create([
             'name' => $data['name'],
             'password' => Hash::make($data['password']),
