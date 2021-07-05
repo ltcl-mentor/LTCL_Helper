4cd5bb50474bcf74eb095e8f2295ada07f17f654
KengoToyoda <toyoken1710@gmail.com>
2021-07-05 14:17:40 +0900

    デザイン変更(生徒用ページのみ)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--css responsible-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=yes">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
