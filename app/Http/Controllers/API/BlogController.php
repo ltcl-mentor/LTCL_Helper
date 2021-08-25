f3e98cfad21c4e506578660a81fe789a38aab9a5
ltcl-mentor <tomoki.koyama.levtechcollege@gmail.com>
2021-08-25 16:13:39 +0900

    API試作

---
+++ app/Http/Controllers/API/BlogController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Question;
+use App\Http\Controllers\Controller;
+
+class BlogController extends Controller
+{
+    public function blog()
+    {
+        return Question::where('category', 0)->get();
+    }
+}
