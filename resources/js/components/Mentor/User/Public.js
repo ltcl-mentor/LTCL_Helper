04a36d18bb91104391a415a51c557ca223fe1c82
ltcl-mentor <tomoki.koyama.levtechcollege@gmail.com>
2021-09-24 00:49:23 +0900

    受講生登録のReactコンポーネント修正

--- resources/js/components/Mentor/User/Public.js
+++ resources/js/components/Mentor/User/Public.js
@@ -1,93 +1,85 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import Forms from './forms';
 
-class Public extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            year: '',
-            month: '',
-            number: '',
-        };
-    }
- 
-    setYear(event) {
-        this.setState({ year: event.target.value });
-    }
+function Public() {
+    const [year, setYear] = useState('');
+    const [month, setMonth] = useState('');
+    const [number, setNumber] = useState('');
+
+    const handleYear = (event) => {
+        setYear(event.target.value);
+    };
     
-    setMonth(event) {
-        this.setState({ month: event.target.value });
-    }
+    const handleMonth = (event) => {
+        setMonth(event.target.value);
+    };
     
-    setNumber(event) {
-        this.setState({ number: event.target.value });
-    }
+    const handleNumber = (event) => {
+        setNumber(event.target.value);
+    };
     
-    render(){
-        var today = new Date();
-        let year = today.getFullYear();
-        let month = today.getMonth();
-        
-        return (
-            <div className="card-body">
-                <div>
-                    <select onChange={(event) => { this.setYear(event) }} required>
-                        <option value="">選択してください。</option>
-                        <option value={ year-1 }>{ year-1 }</option>
-                        <option value={ year }>{ year }</option>
-                        <option value={ year+1 }>{ year+1 }</option>
-                    </select>
-                    年
-                    <select onChange={(event) => { this.setMonth(event) }} required>
-                        <option value="">選択してください。</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                    </select>
-                    月入学者を
-                    <select onChange={(event) => { this.setNumber(event) }} require>
-                        <option value="">選択してください。</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                    </select>
-                    名登録する
-                    <div className="empty"></div>
-                </div>
-                <Forms
-                    password={ 'ltcl'+this.state.year%100+('0'+this.state.month).slice(-2) }
-                    number={ this.state.number } makeForm={ this.state.makeForm }
-                />
+    var today = new Date();
+    let thisYear = today.getFullYear();
+    
+    return (
+        <div className="card-body">
+            <div>
+                <select onChange={(event) => { handleYear(event) }} required>
+                    <option value="">選択してください。</option>
+                    <option value={ thisYear-1 }>{ thisYear-1 }</option>
+                    <option value={ thisYear }>{ thisYear }</option>
+                    <option value={ thisYear+1 }>{ thisYear+1 }</option>
+                </select>
+                年
+                <select onChange={(event) => { handleMonth(event) }} required>
+                    <option value="">選択してください。</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                </select>
+                月入学者を
+                <select onChange={(event) => { handleNumber(event) }} require>
+                    <option value="">選択してください。</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                </select>
+                名登録する
+                <div className="empty"></div>
             </div>
-        );
-    }
+            <Forms
+                password={ 'ltcl'+year%100+('0'+month).slice(-2) }
+                number={ number }
+            />
+        </div>
+    );
 }
 
 export default Public;
--- resources/js/components/Mentor/User/forms.js
+++ resources/js/components/Mentor/User/forms.js
@@ -1,74 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {useState} from 'react';
 
-class Forms extends React.Component{
-    constructor(props){
-        super(props);
-        let csrf_token = document.head.querySelector('meta[name="csrf-token"]').content;
-        this.state={
-            csrf_token: csrf_token,
-            set: 0,
-        };
-    }
+function Forms(props) {
+    const csrf_token = document.head.querySelector('meta[name="csrf-token"]').content;
+    const [clickCount, setClickCount] = useState(0);
 
-    doubleClickStopper() {
-        if( this.state.set === 0){
-            this.setState({ set: 1 });
+    const doubleClickStopper = () => {
+        if( clickCount === 0){
+            setClickCount(1);
             document.getElementById('create').submit();
         }else{
             return false;
         }
+    };
+    
+    let formCount;
+    let forms = [];
+    for(formCount=1; formCount<=props.number; formCount++) {
+        forms.push(
+            <div>
+                <div className="form-group row">
+                    <label for="name" className="col-md-4 col-form-label text-md-right">name{ formCount }</label>
+
+                    <div className="col-md-6">
+                        <input id="name" type="text" className="form-control" name={ `name`+formCount } required autoComplete="name"/>
+                    </div>
+                </div>
+                
+                <div className="form-group row">
+                    
+
+                    <div className="col-md-6">
+                        <input id="password" type="hidden" className="form-control" name="password" required autoComplete="new-password" value={ props.password }/>
+                        <input type="hidden" name="password_confirmation" value={ props.password }/>
+                    </div>
+                </div>
+            </div>
+        ); 
     }
     
-    render(){
-        var i;
-        let forms = [];
-        for(i=1;i<=this.props.number;i++) {
-            forms.push(
-                <div>
-                    <div className="form-group row">
-                        <label for="name" className="col-md-4 col-form-label text-md-right">name{ i }</label>
-
-                        <div className="col-md-6">
-                            <input id="name" type="text" className="form-control" name={`name`+i} required autoComplete="name"/>
-                        </div>
-                    </div>
-                    
-                    <div className="form-group row">
-                        
-
-                        <div className="col-md-6">
-                            <input id="password" type="hidden" className="form-control" name="password" required autoComplete="new-password" value={ this.props.password }/>
-                            <input type="hidden" name="password_confirmation" value={ this.props.password }/>
-                        </div>
-                    </div>
+    let submitBtn;
+    if(props.number){
+        submitBtn = (
+             <div className="form-group row mb-0">
+                <div className="col-md-6 offset-md-4">
+                    <p className="btn btn-primary" onClick={() => { doubleClickStopper() }}>
+                        Register
+                    </p>
                 </div>
-            ); 
-        }
-        
-        let submitBtn;
-        if(this.props.number){
-            submitBtn = (
-                 <div className="form-group row mb-0">
-                    <div className="col-md-6 offset-md-4">
-                        <p className="btn btn-primary" onClick={() => { this.doubleClickStopper() }}>
-                            Register
-                        </p>
-                    </div>
-                </div>
-            );
-        }
-        
-        return (
-            <div>
-                <form method="POST" action="/users/public/register" id="create">
-                    <input type="hidden" name="_token" value={ this.state.csrf_token }/>
-                    { forms }
-                    { submitBtn }
-                </form>
             </div>
         );
     }
+    
+    return (
+        <div>
+            <form method="POST" action="/users/public/register" id="create">
+                <input type="hidden" name="_token" value={ csrf_token }/>
+                { forms }
+                { submitBtn }
+            </form>
+        </div>
+    );
 }
 
 export default Forms;
