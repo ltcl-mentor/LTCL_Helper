7f66b963b9c7e41ee1e020b58cb53250228e7987
ltcl-mentor <tomoki.koyama.levtechcollege@gmail.com>
2021-09-24 21:34:45 +0900

    home画面変更中

--- resources/js/components/Public/Home/Home.js
+++ resources/js/components/Public/Home/Home.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from "axios";
+import isWeekend from 'date-fns/isWeekend';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import StaticDatePicker from '@mui/lab/StaticDatePicker';
 
 function Home() {
+    const [value, setValue] = React.useState(new Date());
+    
     return (
-        <div>OK</div>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <StaticDatePicker
+                orientation="landscape"
+                openTo="day"
+                value={value}
+                shouldDisableDate={isWeekend}
+                onChange={(newValue) => {
+                    setValue(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+            />
+        </LocalizationProvider>
     );
 }
 
