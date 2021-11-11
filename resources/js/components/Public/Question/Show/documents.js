7b15912d3ab970a2ff6c5505f32a1515247292d5
ltcl-mentor <tomoki.koyama.levtechcollege@gmail.com>
2021-11-12 05:17:11 +0900

    管理者の質問詳細画面のデザイン変更

--- resources/js/components/Public/Question/Show/documents.js
+++ resources/js/components/Public/Question/Show/documents.js
@@ -4,6 +4,9 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Chip from '@mui/material/Chip';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
 function Document(props) {
     let documents;
@@ -12,32 +15,38 @@ function Document(props) {
     } else {
         documents = props.documents.map((document) => {
             return (
-                <div>
-                    <CardActionArea>
+                <Grid item >
+                    <CardActionArea sx={{ width: "300px", height: "280px"}}>
                         <a href={ document.link } target="_blank">
-                            <Card className="document">
-                                <img className="document_img" src="/images/NotePM_Logo_Vertical.png" height="150px" width="200px"/>
+                            <Card sx={{ width: "300px", height: "280px"}}>
+                                <CardMedia
+                                    component="img"
+                                    height="100"
+                                    image="/images/NotePM_Logo_Vertical.png"
+                                />
                                 <CardContent>
                                     { document.beginner === 1 ? <Chip variant="outlined" label="初心者向け" /> : "" }
                                     { document.amature === 1 ? <Chip label="中級者向け" /> : "" }
                                     { document.master === 1 ? <Chip label="上級者向け" /> : "" }
                                     { document.all === 1 ? <Chip label="全員向け" /> : "" }
-                                    <Typography gutterBottom variant="h5" component="h2" align="center">
+                                    <Typography gutterBottom variant="h6" component="div" align="center">
                                         { document.title }
                                     </Typography>
                                 </CardContent>
                             </Card>
                         </a>
                     </CardActionArea>
-                </div>
+                </Grid>
             );
         });
     }
     
     return (
-        <div className="documents">
-            { documents }
-        </div>
+        <Box sx={{marginTop:2, marginBottom: 3}}>
+            <Grid container spacing={2} sx={{ flexGrow: 1 }} justifyContent="center" >
+                { documents }
+            </Grid>
+        </Box>
     );
 }
 
