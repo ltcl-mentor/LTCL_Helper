e3f3e73da594e437d21aa94c7b7a9fffec4956f5
sho watanabe <showatanabe0226@gmail.com>
2022-04-07 01:53:10 +0900

    [update]プレビューと画像投稿の順番を入れ替え

--- resources/js/components/Public/Question/Create/Create/question-form/original-text-form/originalTextForm.js
+++ resources/js/components/Public/Question/Create/Create/question-form/original-text-form/originalTextForm.js
@@ -1,17 +1,17 @@
-import React, {useState, useEffect} from 'react';
-import Typography from '@material-ui/core/Typography';
-import Box from '@mui/material/Box';
-import TextareaAutosize from '@mui/material/TextareaAutosize';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import ToggleButton from '@mui/material/ToggleButton';
-import CodeIcon from '@mui/icons-material/Code';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
-import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
-import TextField from '@mui/material/TextField';
+import React, { useState, useEffect } from "react";
+import Typography from "@material-ui/core/Typography";
+import Box from "@mui/material/Box";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import ToggleButton from "@mui/material/ToggleButton";
+import CodeIcon from "@mui/icons-material/Code";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
+import TextField from "@mui/material/TextField";
 
-import Picture from './picture';
-import Preview from './preview';
+import Picture from "./picture";
+import Preview from "./preview";
 
 /**
  * 自作入力フォームのメインコンポーネント
@@ -19,133 +19,159 @@ import Preview from './preview';
 function TextForm(props) {
     const [value, setValue] = useState(0);
     const [selectionRange, setSelectionRange] = useState(() => [0, 0]);
-    const [image, setImage] = useState('');
+    const [image, setImage] = useState("");
     const [cursor, setCursor] = useState(0);
-    
+
     // textareaの特定位置にカーソルを持っていきたいときに実行
     // setSelectionRangeに連動
     useEffect(() => {
         // textareaを入力状態に
-        document.getElementById('text').focus();
+        document.getElementById("text").focus();
         // カーソルを移動
-        document.getElementById('text').setSelectionRange(selectionRange[0], selectionRange[1]);
-    },[selectionRange]);
-    
+        document
+            .getElementById("text")
+            .setSelectionRange(selectionRange[0], selectionRange[1]);
+    }, [selectionRange]);
+
     useEffect(() => {
         if (image.length > 0) {
             setValue(0);
-            
-             // カーソルの直前
+
+            // カーソルの直前
             let selection_before = props.text.substr(0, cursor);
             // カーソルの直後
             let selection_after = props.text.substr(cursor);
-        
+
             // カーソル位置の直後に画像リンク挿入
-            props.setText(selection_before + '\n<image link=' + image + '//>\n' + selection_after);
-        
+            props.setText(
+                selection_before +
+                    "\n<image link=" +
+                    image +
+                    "//>\n" +
+                    selection_after
+            );
+
             // カーソル位置を画像の前に
             setSelectionRange([cursor, cursor]);
-            
-            setImage('');
+
+            setImage("");
         }
     }, [image]);
-    
+
     // コードブロッック
     const handleCode = () => {
         // カーソル位置取得
-        let selection_start = document.getElementById('text').selectionStart;
-        let selection_end = document.getElementById('text').selectionEnd;
-        
+        let selection_start = document.getElementById("text").selectionStart;
+        let selection_end = document.getElementById("text").selectionEnd;
+
         // 挿入位置の調整
-        if (props.text.slice(-2) === '\n') {
+        if (props.text.slice(-2) === "\n") {
             props.setText(props.text.split(-2));
         }
-        
+
         // カーソル（選択部分）の直前
         let selection_before = props.text.substr(0, selection_start);
         // 選択部分（選択時）
         let selection = props.text.substr(selection_start, selection_end);
         // カーソル（選択部分）の直後
         let selection_after = props.text.substr(selection_end);
-        
+
         // 文字列の選択がされていなかった場合
         if (selection_start === selection_end) {
             // カーソル位置の直後に記号挿入
-            props.setText(selection_before + '\n```\n \n```\n' + selection_after);
-            
+            props.setText(
+                selection_before + "\n```\n \n```\n" + selection_after
+            );
+
             // カーソル位置を```の間に
             setSelectionRange([selection_end + 6, selection_end + 6]);
-        
-        // 文字列の選択がされていた場合
+
+            // 文字列の選択がされていた場合
         } else {
             // 選択部分の前後に記号挿入
-            props.setText(selection_before + '\n```\n' + selection + '```\n' + selection_after);
-            
+            props.setText(
+                selection_before +
+                    "\n```\n" +
+                    selection +
+                    "```\n" +
+                    selection_after
+            );
+
             // カーソル位置をコードブロックの後ろに
             setSelectionRange([selection_end + 10, selection_end + 10]);
         }
     };
-    
+
     // 通常の箇条書き
     const handleList = () => {
         // カーソル位置取得
-        let selection_start = document.getElementById('text').selectionStart;
-        let selection_end = document.getElementById('text').selectionEnd;
-        
+        let selection_start = document.getElementById("text").selectionStart;
+        let selection_end = document.getElementById("text").selectionEnd;
+
         // 挿入位置の調整
-        if (props.text.slice(-2) === '\n') {
+        if (props.text.slice(-2) === "\n") {
             props.setText(props.text.split(-2));
         }
-        
+
         // カーソル（選択部分）の直前
         let selection_before = props.text.substr(0, selection_start);
         // 選択部分（選択時）
         let selection = props.text.substr(selection_start, selection_end);
         // カーソル（選択部分）の直後
         let selection_after = props.text.substr(selection_end);
-        
+
         // 文字列の選択がされていなかった場合
         if (selection_start === selection_end) {
             // カーソル位置の直後に記号挿入
-            props.setText(selection_before + '\n* ' + selection_after);
-            
+            props.setText(selection_before + "\n* " + selection_after);
+
             // カーソル位置を* の後ろに
             setSelectionRange([selection_end + 3, selection_end + 3]);
-        
-        // 文字列の選択がされていた場合
+
+            // 文字列の選択がされていた場合
         } else {
             // 選択部分の前後に記号挿入
-            props.setText(selection_before + '\n* ' + selection + '\n' + selection_after);
-            
+            props.setText(
+                selection_before + "\n* " + selection + "\n" + selection_after
+            );
+
             // カーソル位置を箇条書きの後ろに
             setSelectionRange([selection_end + 5, selection_end + 5]);
         }
     };
-    
+
     // 質問の記録
-    const handleText = (event) => {
-        setCursor(document.getElementById('text').selectionStart);
+    const handleText = event => {
+        setCursor(document.getElementById("text").selectionStart);
         props.setText(event.target.value);
     };
-    
+
     // タブの切り替え
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
-    
+
     // タブの要素
     let tab_content;
     if (value === 0) {
         tab_content = (
             <React.Fragment>
                 <Box sx={{ textAlign: "center", marginBottom: 1 }}>
-                    <ToggleButton value="bold" aria-label="code_block" onClick={ handleCode }>
+                    <ToggleButton
+                        value="bold"
+                        aria-label="code_block"
+                        onClick={handleCode}
+                    >
                         <CodeIcon />
                     </ToggleButton>
-                    <ToggleButton value="italic" aria-label="list" onClick={ handleList }>
+                    <ToggleButton
+                        value="italic"
+                        aria-label="list"
+                        onClick={handleList}
+                    >
                         <FormatListBulletedIcon />
                     </ToggleButton>
-                        {/*<ToggleButton value="underlined" aria-label="number_list">
+                    {/*<ToggleButton value="underlined" aria-label="number_list">
                             <FormatListNumberedIcon />
                         </ToggleButton>*/}
                 </Box>
@@ -156,48 +182,48 @@ function TextForm(props) {
                     helperText={props.validationMessage}
                     multiline
                     rows={15}
-                    value={ props.text }
-                    onChange={ (event) => handleText(event) }
-                    style={{ 
+                    value={props.text}
+                    onChange={event => handleText(event)}
+                    style={{
                         width: "90%",
                         marginLeft: "5%",
-                        paddingTop:2,
+                        paddingTop: 2
                     }}
                     id="text"
                 />
             </React.Fragment>
         );
     } else if (value === 1) {
-        tab_content = (<Preview text={ props.text } />);
-    } else if (value === 2) {
         tab_content = (
             <Picture
-                setImage={ setImage }
-                images={ props.images }
-                setImages={ props.setImages }
+                setImage={setImage}
+                images={props.images}
+                setImages={props.setImages}
             />
         );
+    } else if (value === 2) {
+        tab_content = <Preview text={props.text} />;
     }
-    
+
     return (
         <div>
             <Box sx={{ marginTop: 2, width: "90%", marginLeft: "5%" }}>
                 <Tabs
-                    value={ value }
-                    onChange={ handleChange }
+                    value={value}
+                    onChange={handleChange}
                     aria-label="basic tabs example"
-                    variant={ window.innerWidth > 350 ? "standard" : "scrollable"}
-                    scrollButtons={ false }
+                    variant={
+                        window.innerWidth > 350 ? "standard" : "scrollable"
+                    }
+                    scrollButtons={false}
                     centered
                 >
                     <Tab label="入力" />
-                    <Tab label="プレビュー" />
                     <Tab label="画像添付" />
+                    <Tab label="プレビュー" />
                 </Tabs>
-                
-                <Box sx={{ marginTop: 2 }}>
-                    { tab_content }
-                </Box>
+
+                <Box sx={{ marginTop: 2 }}>{tab_content}</Box>
             </Box>
         </div>
     );
