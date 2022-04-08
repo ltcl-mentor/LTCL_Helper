c1c3f0c3846c3c5c7d5d6e5a38895dc79beaa290
sho watanabe <showatanabe0226@gmail.com>
2022-04-08 12:39:52 +0900

    [update]デザインの一部修正

--- resources/js/components/Public/Question/Show/Show.js
+++ resources/js/components/Public/Question/Show/Show.js
@@ -1,18 +1,18 @@
-import React, {useState, useEffect} from 'react';
-import {useParams, useHistory, useLocation} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams, useHistory, useLocation } from "react-router-dom";
 import axios from "axios";
-import Typography from '@material-ui/core/Typography';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+import Typography from "@material-ui/core/Typography";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
-import Alert from '../../../Alert';
-import Breadcrumbs from '../../../Breadcrumbs';
-import Parameters from './parameters';
-import Question from './question';
-import Comments from './comments/comments';
-import Documents from './documents';
-import RelatedQuestions from './related-questions';
+import Alert from "../../../Alert";
+import Breadcrumbs from "../../../Breadcrumbs";
+import Parameters from "./parameters";
+import Question from "./question";
+import Comments from "./comments/comments";
+import Documents from "./documents";
+import RelatedQuestions from "./related-questions";
 
 /**
  * 質問詳細画面(公開)のメインコンポーネント
@@ -27,125 +27,142 @@ function Show() {
     const [user, setUser] = useState([]);
     const [comment_changing, setCommentChanging] = useState(false);
     const [question_changing, setQuestionChanging] = useState(false);
-    
+
     // 画面幅を随時取得
-    window.addEventListener('resize', function() {
+    window.addEventListener("resize", function() {
         setScreenWidth(window.innerWidth);
     });
-    
+
     // 画面描画時に実行
     useEffect(() => {
         // ユーザの質問詳細画面の閲覧を記録
         axios
-            .post(`/questions/${ id }/record`)
-            .then(response => {
-                
-            }).catch(error => {
+            .post(`/questions/${id}/record`)
+            .then(response => {})
+            .catch(error => {
                 console.log(error);
             });
-        
+
         // 個別質問を取得
         axios
-            .get(`/react/question/checked/${ id }`)
+            .get(`/react/question/checked/${id}`)
             .then(response => {
                 setQuestion(response.data);
-            }).catch(error => {
+            })
+            .catch(error => {
                 console.log(error);
             });
-        
+
         // 質問に関連する全参考記事を取得
         axios
-            .get(`/react/documents/related/${ id }`)
+            .get(`/react/documents/related/${id}`)
             .then(response => {
                 setDocuments(response.data);
-            }).catch(error => {
+            })
+            .catch(error => {
                 console.log(error);
             });
-        
+
         // ログインユーザ情報を取得
         axios
             .get("/react/user")
             .then(response => {
                 setUser(response.data);
-            }).catch(error => {
+            })
+            .catch(error => {
                 console.log(error);
             });
     }, []);
-    
+
     useEffect(() => {
-        if (!(comment_changing)) {
+        if (!comment_changing) {
             // 個別質問を取得
             axios
-                .get(`/react/question/checked/${ id }`)
+                .get(`/react/question/checked/${id}`)
                 .then(response => {
                     setQuestion(response.data);
-                }).catch(error => {
+                })
+                .catch(error => {
                     console.log(error);
                 });
         }
     }, [comment_changing, question_changing]);
-    
+
     useEffect(() => {
         if (question) {
-        // この質問と同じカテゴリー、トピックの質問を取得
-        axios
-            .get(`/react/questions/search?category=${ question.category }&topic=${ question.topic }`)
-            .then(response => {
-                setRelatedQuestions(response.data);
-            }).catch(error => {
-                console.log(error);
-            });
+            // この質問と同じカテゴリー、トピックの質問を取得
+            axios
+                .get(
+                    `/react/questions/search?category=${question.category}&topic=${question.topic}`
+                )
+                .then(response => {
+                    setRelatedQuestions(response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         }
     }, [question]);
-    
+
     const handleResolved = () => {
-        if (confirm('一度解決扱いにすると今後変更できません。\nよろしいですか？')) {
+        if (
+            confirm(
+                "一度解決扱いにすると今後変更できません。\nよろしいですか？"
+            )
+        ) {
             setQuestionChanging(true);
-            
+
             axios
-                .post(`/questions/${ id }/resolved`)
+                .post(`/questions/${id}/resolved`)
                 .then(response => {
                     if (response.status === 200) {
                         setQuestionChanging(false);
                     }
-                }).catch(error => {
+                })
+                .catch(error => {
                     console.log(error);
                 });
         } else {
-            window.alert('キャンセルしました');
+            window.alert("キャンセルしました");
             return false;
         }
     };
-    
+
     return (
         <div className="container">
             <Alert
                 type="question"
-                status={ parameter.state && parameter.state.question }
-                info={ parameter.state && parameter.state.number }
+                status={parameter.state && parameter.state.question}
+                info={parameter.state && parameter.state.number}
             />
-            
-            <Breadcrumbs page="public_question_show"/>
-            
-            { (question.length !== 0 && !(question.is_resolved) && (question.user_id === user.id || user.is_admin === 'staff')) &&
-                <Typography component="div" align="center" sx={{ marginTop: 4, marginBottom: 3 }} >
-                    <Button
-                        variant="contained"
-                        color="success"
-                        onClick={ handleResolved }
+
+            <Breadcrumbs page="public_question_show" />
+
+            {question.length !== 0 &&
+                !question.is_resolved &&
+                (question.user_id === user.id || user.is_admin === "staff") && (
+                    <Typography
+                        component="div"
+                        align="center"
+                        sx={{ marginTop: 4, marginBottom: 3 }}
                     >
-                        解決！！
-                    </Button>
-                </Typography>
-            }
-            
-            <Parameters 
-                category={ question.category }
-                topic={ question.topic }
-                curriculum_number={ question.curriculum_number }
-                is_resolved={ question.is_resolved }
+                        <Button
+                            variant="contained"
+                            color="success"
+                            onClick={handleResolved}
+                        >
+                            解決！！
+                        </Button>
+                    </Typography>
+                )}
+
+            <Parameters
+                category={question.category}
+                topic={question.topic}
+                curriculum_number={question.curriculum_number}
+                is_resolved={question.is_resolved}
             />
-            
+
             <Grid container spacing={2} sx={{ flexGrow: 1 }}>
                 <Grid
                     item
@@ -155,39 +172,37 @@ function Show() {
                 >
                     <Box>
                         <Question
-                            title={ question.title }
-                            remarks={ question.remarks }
-                            updated_at={ question.updated_at }
-                            question={ question.question }
+                            title={question.title}
+                            remarks={question.remarks}
+                            updated_at={question.updated_at}
+                            question={question.question}
                         />
-                        
+
                         <Comments
-                            main_comments={ question.main_comments }
-                            sub_comments={ question.sub_comments }
-                            question_id={ id }
-                            setCommentChanging={ setCommentChanging }
-                            user_id={ user.id }
-                            is_admin={ user.is_admin }
+                            main_comments={question.main_comments}
+                            sub_comments={question.sub_comments}
+                            question_id={id}
+                            setCommentChanging={setCommentChanging}
+                            user_id={user.id}
+                            is_admin={user.is_admin}
                         />
-                        
+
                         <Typography
                             variant="h4"
                             component="div"
                             align="center"
                             sx={{
                                 marginTop: 4,
-                                marginBottom: 2,
+                                marginBottom: 2
                             }}
                         >
                             参考記事
                         </Typography>
-                        
-                        <Documents 
-                            documents={ documents }
-                        />
+
+                        <Documents documents={documents} />
                     </Box>
                 </Grid>
-                
+
                 <Grid
                     item
                     sx={{
@@ -195,14 +210,11 @@ function Show() {
                         minWidth: "300px"
                     }}
                 >
-                    <RelatedQuestions 
-                        related_questions={ related_questions }
-                    />
+                    <RelatedQuestions related_questions={related_questions} />
                 </Grid>
             </Grid>
         </div>
     );
-    
 }
 
 export default Show;
--- resources/js/components/Route.js
+++ resources/js/components/Route.js
@@ -1,180 +1,269 @@
-import React, {useState, useEffect, createContext} from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useEffect, createContext } from "react";
+import ReactDOM from "react-dom";
 import axios from "axios";
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Bar from './Layout/Bar';
-import AccessError from './Error';
-import MyPage from './Public/User/MyPage';
-import MyQuestion from './Public/User/QuestionShow';
-import History from './Public/History/History';
-import Home from './Public/Home/Home';
-import PublicDocumentIndex from './Public/Document/Index/Index';
-import PublicQuestionIndex from './Public/Question/Index/Index';
-import PublicQuestionCreate from './Public/Question/Create/Create/Create';
-import PublicQuestionShow from './Public/Question/Show/Show';
-import Condition from './Public/Search/Condition/Condition';
-import Freeword from './Public/Search/Freeword/Freeword';
-import Contact from './Public/Contact/Contact';
-import MentorTop from './Mentor/Top';
-import DocumentIndex from './Mentor/Document/Index/Index';
-import DocumentShow from './Mentor/Document/Show/Show';
-import DocumentCreate from './Mentor/Document/Create/Create';
-import DocumentEdit from './Mentor/Document/Edit/Edit';
-import QuestionIndex from './Mentor/Question/Index/Index/Index';
-import QuestionMentorYetCommentIndex from './Mentor/Question/Index/yet-comment-index/FromMentor';
-import QuestionStudentYetCommentIndex from './Mentor/Question/Index/yet-comment-index/FromStudent';
-import QuestionShow from './Mentor/Question/Show/Show';
-import QuestionEdit from './Mentor/Question/Edit/Edit';
-import LinkFromQuestionIndex from './Mentor/Link/fromQuestion/Index/Index';
-import LinkFromQuestionShow from './Mentor/Link/fromQuestion/Show/Show';
-import LinkFromDocumentIndex from './Mentor/Link/fromDocument/Index/Index';
-import LinkFromDocumentShow from './Mentor/Link/fromDocument/Show/Show';
-import UserIndex from './Mentor/User/Index/Index';
-import UserRegisterPublic from './Mentor/User/Register/Public/Public';
-import UserRegisterAdmin from './Mentor/User/Register/Admin/Admin';
-import EventIndex from './Mentor/Event/Index/Index';
-import EventRegister from './Mentor/Event/Create/Create';
+import Bar from "./Layout/Bar";
+import AccessError from "./Error";
+import MyPage from "./Public/User/MyPage";
+import MyQuestion from "./Public/User/QuestionShow";
+import History from "./Public/History/History";
+import Home from "./Public/Home/Home";
+import PublicDocumentIndex from "./Public/Document/Index/Index";
+import PublicQuestionIndex from "./Public/Question/Index/Index";
+import PublicQuestionCreate from "./Public/Question/Create/Create/Create";
+import PublicQuestionShow from "./Public/Question/Show/Show";
+import Condition from "./Public/Search/Condition/Condition";
+import Freeword from "./Public/Search/Freeword/Freeword";
+import Contact from "./Public/Contact/Contact";
+import MentorTop from "./Mentor/Top";
+import DocumentIndex from "./Mentor/Document/Index/Index";
+import DocumentShow from "./Mentor/Document/Show/Show";
+import DocumentCreate from "./Mentor/Document/Create/Create";
+import DocumentEdit from "./Mentor/Document/Edit/Edit";
+import QuestionIndex from "./Mentor/Question/Index/Index/Index";
+import QuestionMentorYetCommentIndex from "./Mentor/Question/Index/yet-comment-index/FromMentor";
+import QuestionStudentYetCommentIndex from "./Mentor/Question/Index/yet-comment-index/FromStudent";
+import QuestionShow from "./Mentor/Question/Show/Show";
+import QuestionEdit from "./Mentor/Question/Edit/Edit";
+import LinkFromQuestionIndex from "./Mentor/Link/fromQuestion/Index/Index";
+import LinkFromQuestionShow from "./Mentor/Link/fromQuestion/Show/Show";
+import LinkFromDocumentIndex from "./Mentor/Link/fromDocument/Index/Index";
+import LinkFromDocumentShow from "./Mentor/Link/fromDocument/Show/Show";
+import UserIndex from "./Mentor/User/Index/Index";
+import UserRegisterPublic from "./Mentor/User/Register/Public/Public";
+import UserRegisterAdmin from "./Mentor/User/Register/Admin/Admin";
+import EventIndex from "./Mentor/Event/Index/Index";
+import EventRegister from "./Mentor/Event/Create/Create";
 
 export const LoginUser = createContext();
 
 function Router() {
     const [user, setUser] = useState([]);
-    
+
     useEffect(() => {
         // ログインユーザー情報取得
         axios
-            .get('/react/user')
+            .get("/react/user")
             .then(response => {
                 setUser(response.data);
-            }).catch(error => {
+            })
+            .catch(error => {
                 console.log(error);
             });
     }, []);
-    
+
     let user_links;
-    
+
     let admin_links;
-    
+
     if (user.length !== 0) {
         // 一般ユーザがアクセスできURL
         user_links = (
             <Switch>
                 {/* トップ画面表示 */}
-                <Route key="home" path="/" exact component={ Home }/>
-                
+                <Route key="home" path="/" exact component={Home} />
+
                 {/* ユーザマイページ表示
                 <Route path="/topic/:id" exact component={ MyPage }/>*/}
-                
+
                 {/* ユーザマイページ表示 */}
-                <Route path="/my_page" exact component={ MyPage }/>
-                
+                <Route path="/my_page" exact component={MyPage} />
+
                 {/* ユーザマイページ質問詳細表示 */}
-                <Route path="/my_page/questions/:id" exact component={ MyQuestion }/>
-                
+                <Route
+                    path="/my_page/questions/:id"
+                    exact
+                    component={MyQuestion}
+                />
+
                 {/* 質問履歴画面表示 */}
-                <Route path="/history" exact component={ History }/>
-                
+                <Route path="/history" exact component={History} />
+
                 {/* 公開中の参考記事一覧表示 */}
-                <Route path="/public/documents/index" exact component={ PublicDocumentIndex }/>
-                
+                <Route
+                    path="/public/documents/index"
+                    exact
+                    component={PublicDocumentIndex}
+                />
+
                 {/* 公開中の質問一覧表示 */}
-                <Route path="/public/questions/index" exact component={ PublicQuestionIndex }/>
-                
+                <Route
+                    path="/public/questions/index"
+                    exact
+                    component={PublicQuestionIndex}
+                />
+
                 {/* 受講生の質問投稿画面表示 */}
-                <Route path="/public/questions/create" exact component={ PublicQuestionCreate }/>
-                
+                <Route
+                    path="/public/questions/create"
+                    exact
+                    component={PublicQuestionCreate}
+                />
+
                 {/* 質問詳細画面表示 */}
-                <Route path="/public/questions/:id" exact component={ PublicQuestionShow }/>
-                
+                <Route
+                    path="/public/questions/:id"
+                    exact
+                    component={PublicQuestionShow}
+                />
+
                 {/* 絞り込み検索画面表示 */}
-                <Route path="/search/condition" exact component={ Condition }/>
-                
+                <Route path="/search/condition" exact component={Condition} />
+
                 {/* フリーワード検索画面表示 */}
-                <Route path="/search/freeword" exact component={ Freeword }/>
-                
+                <Route path="/search/freeword" exact component={Freeword} />
+
                 {/* お問い合わせ画面表示 */}
-                <Route path="/contact" exact component={ Contact }/>
+                <Route path="/contact" exact component={Contact} />
             </Switch>
         );
-        
+
         if (user.is_admin === "staff") {
             // 管理者ユーザがアクセスできるURL
             admin_links = (
                 <Switch>
                     {/* メンター管理画面表示 */}
-                    <Route path="/mentor/top" exact component={ MentorTop }/>
-                    
+                    <Route path="/mentor/top" exact component={MentorTop} />
+
                     {/* 記事一覧画面表示示 */}
-                    <Route path="/documents/index" exact component={ DocumentIndex }/>
-                    
+                    <Route
+                        path="/documents/index"
+                        exact
+                        component={DocumentIndex}
+                    />
+
                     {/* 記事新規作成画面表示 */}
-                    <Route path="/documents/create" exact component={ DocumentCreate }/>
-                    
+                    <Route
+                        path="/documents/create"
+                        exact
+                        component={DocumentCreate}
+                    />
+
                     {/* 記事編集画面表示 */}
-                    <Route path="/documents/:id/edit" exact component={ DocumentEdit }/>
-                    
+                    <Route
+                        path="/documents/:id/edit"
+                        exact
+                        component={DocumentEdit}
+                    />
+
                     {/* 記事詳細画面表示 */}
-                    <Route path="/documents/:id" exact component={ DocumentShow }/>
-                    
+                    <Route
+                        path="/documents/:id"
+                        exact
+                        component={DocumentShow}
+                    />
+
                     {/* 質問一覧画面表示 */}
-                    <Route path="/questions/index" exact component={ QuestionIndex }/>
-                    
+                    <Route
+                        path="/questions/index"
+                        exact
+                        component={QuestionIndex}
+                    />
+
                     {/* メンターコメント待ちの質問一覧画面表示 */}
-                    <Route path="/questions/index/mentor_yet_comment" exact component={ QuestionMentorYetCommentIndex }/>
-                    
+                    <Route
+                        path="/questions/index/mentor_yet_comment"
+                        exact
+                        component={QuestionMentorYetCommentIndex}
+                    />
+
                     {/* 受講生コメント待ちの質問一覧画面表示 */}
-                    <Route path="/questions/index/student_yet_comment" exact component={ QuestionStudentYetCommentIndex }/>
-                    
+                    <Route
+                        path="/questions/index/student_yet_comment"
+                        exact
+                        component={QuestionStudentYetCommentIndex}
+                    />
+
                     {/* 質問新規作成画面表示 */}
                     {/*<Route path="/questions/create" exact component={ QuestionCreate }/>*/}
-                    
+
                     {/* 質問編集画面表示 */}
-                    <Route path="/questions/:id/edit" exact component={ QuestionEdit }/>
-                    
+                    <Route
+                        path="/questions/:id/edit"
+                        exact
+                        component={QuestionEdit}
+                    />
+
                     {/* 質問詳細画面表示 */}
-                    <Route path="/questions/:id" exact component={ QuestionShow }/>
-                    
+                    <Route
+                        path="/questions/:id"
+                        exact
+                        component={QuestionShow}
+                    />
+
                     {/* 紐付け（単体質問と複数記事）一覧画面表示 */}
-                    <Route path="/links/question/index" exact component={ LinkFromQuestionIndex }/>
-                    
+                    <Route
+                        path="/links/question/index"
+                        exact
+                        component={LinkFromQuestionIndex}
+                    />
+
                     {/* 紐付け（単体質問と複数記事）登録画面表示 */}
-                    <Route path="/links/question/:id" exact component={ LinkFromQuestionShow }/>
-                    
+                    <Route
+                        path="/links/question/:id"
+                        exact
+                        component={LinkFromQuestionShow}
+                    />
+
                     {/* 紐付け（単体記事と複数質問）一覧画面表示 */}
-                    <Route path="/links/document/index" exact component={ LinkFromDocumentIndex }/>
-                    
+                    <Route
+                        path="/links/document/index"
+                        exact
+                        component={LinkFromDocumentIndex}
+                    />
+
                     {/* 紐付け（単体記事と複数質問）登録画面表示 */}
-                    <Route path="/links/document/:id" exact component={ LinkFromDocumentShow }/>
-                    
+                    <Route
+                        path="/links/document/:id"
+                        exact
+                        component={LinkFromDocumentShow}
+                    />
+
                     {/* ユーザ一覧画面表示 */}
-                    <Route path="/users/index" exact component={ UserIndex }/>
-                    
+                    <Route path="/users/index" exact component={UserIndex} />
+
                     {/* 受講生登録画面表示 */}
-                    <Route path="/users/register/public" exact component={ UserRegisterPublic }/>
-                    
+                    <Route
+                        path="/users/register/public"
+                        exact
+                        component={UserRegisterPublic}
+                    />
+
                     {/* 管理者登録画面表示 */}
-                    <Route path="/users/register/admin" exact component={ UserRegisterAdmin }/>
-                    
+                    <Route
+                        path="/users/register/admin"
+                        exact
+                        component={UserRegisterAdmin}
+                    />
+
                     {/* イベント一覧画面表示 */}
-                    <Route path="/events/index" exact component={ EventIndex }/>
-                    
+                    <Route path="/events/index" exact component={EventIndex} />
+
                     {/* イベント登録画面表示 */}
-                    <Route path="/events/register" exact component={ EventRegister }/>
-                    
+                    <Route
+                        path="/events/register"
+                        exact
+                        component={EventRegister}
+                    />
+
                     {/* イベント編集画面表示 */}
-                    <Route path="/events/register/:id" exact component={ EventRegister }/>
+                    <Route
+                        path="/events/register/:id"
+                        exact
+                        component={EventRegister}
+                    />
                 </Switch>
             );
         }
     }
-    
+
     return (
         <BrowserRouter>
-            <LoginUser.Provider value={ user }>
-                <Bar/>
-                { user_links }
-                { admin_links }
+            <LoginUser.Provider value={user}>
+                <Bar />
+                {user_links}
+                {admin_links}
             </LoginUser.Provider>
         </BrowserRouter>
     );
@@ -182,7 +271,6 @@ function Router() {
 
 export default Router;
 
-
-if (document.getElementById('Router')) {
-    ReactDOM.render(<Router />, document.getElementById('Router'));
+if (document.getElementById("Router")) {
+    ReactDOM.render(<Router />, document.getElementById("Router"));
 }
