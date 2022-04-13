99de0481eade200817d8c2ec1509a2c81e306c73
sho watanabe <showatanabe0226@gmail.com>
2022-04-14 03:17:55 +0900

    [update]使用しているボタンをコンポーネント化されたものに変更

--- resources/js/components/Public/Question/Create/Create/confirm.js
+++ resources/js/components/Public/Question/Create/Create/confirm.js
@@ -3,24 +3,11 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { styled } from "@mui/material/styles";
 import Parameters from "../../Show/parameters";
+import ConfirmButton from "../../../../Atom/Button/ConfirmButton";
 
 /**
  * 質問登録前の確認フォーム
  */
-const ConfirmButton = styled(Button)(({ theme }) => ({
-    variant: "outlined",
-    color: "#771af8",
-    border: "2px solid #771af8",
-    fontWeight: "bold",
-    minWidth: 150,
-    maxWidth: 200,
-    marginBottom: 5,
-    fontSize: 15,
-    "&:hover": {
-        backgroundColor: "#771AF8",
-        color: "white"
-    }
-}));
 
 function QuestionConfirm(props) {
     let question = [];
