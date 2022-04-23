c4b01dc7cc35029970e62970b809a5a7ef35940b
sho watanabe <showatanabe0226@gmail.com>
2022-04-23 16:34:27 +0900

    [update]モーダルで改行を保持するように修正

--- resources/js/components/Public/Home/Top/information/information.js
+++ resources/js/components/Public/Home/Top/information/information.js
@@ -1,37 +1,36 @@
-import React, { useState, useEffect, useContext } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
 
-import Grid from '@mui/material/Grid';
-import Typography from '@material-ui/core/Typography';
-import Button from '@mui/material/Button';
+import Grid from "@mui/material/Grid";
+import Typography from "@material-ui/core/Typography";
+import Button from "@mui/material/Button";
 
-import Modals from '../../modal';
-import Infos from './infos';
-import Weather from '../weather';
-import { LoginUser } from '../../../../Route';
-
+import Modals from "../../modal";
+import Infos from "./infos";
+import Weather from "../weather";
+import { LoginUser } from "../../../../Route";
 
 /**
  * お知らせ
  */
-const Information = (props) => {
+const Information = props => {
     const [open, setOpen] = useState(false);
     const [type, setType] = useState("user");
     const [dates, setDates] = useState([]);
     const [infos, setInfos] = useState([]);
     const [events, setEvents] = useState([]);
     const user = useContext(LoginUser);
-    
-    const handleOpen = (type) => {
+
+    const handleOpen = type => {
         setOpen(true);
         setType(type);
     };
-    
+
     const handleClose = () => {
         setOpen(false);
         setType("user");
     };
-    
+
     useEffect(() => {
         axios
             .get(`/react/infos`)
@@ -39,37 +38,57 @@ const Information = (props) => {
                 setInfos(response.data.infos.infos);
                 setDates(response.data.infos.dates);
                 setEvents(response.data.events);
-            }).catch(error => {
+            })
+            .catch(error => {
                 console.log(error);
             });
     }, []);
-    
+
     return (
         <div className="information">
-            <Modals 
-                open={open} 
-                type={type} 
+            <Modals
+                open={open}
+                type={type}
                 handleClose={handleClose}
                 events={events}
                 setInfos={setInfos}
                 setDates={setDates}
             />
-            
-            <Grid columns={16} container sx={{ width: '80%', ml: 'auto', mr: 'auto' }}>
+
+            <Grid
+                columns={16}
+                container
+                sx={{ width: "80%", ml: "auto", mr: "auto" }}
+            >
                 <Grid xs={8} item>
-                    <Typography component="div" sx={{ color: '#771AF8', fontWeight: 'bold', fontSize: 24, pl: 2 }}>
+                    <Typography
+                        component="div"
+                        sx={{
+                            color: "#771AF8",
+                            fontWeight: "bold",
+                            fontSize: 24,
+                            pl: 2
+                        }}
+                    >
                         お知らせ
-                        {user.is_admin == "staff" &&
-                            <Button 
-                                sx={{ verticalAlign: 'top', color: '#771AF8', '&:hover': { backgroundColor: 'white', textDecoration: 'underline' } }}
+                        {user.is_admin == "staff" && (
+                            <Button
+                                sx={{
+                                    verticalAlign: "top",
+                                    color: "#771AF8",
+                                    "&:hover": {
+                                        backgroundColor: "white",
+                                        textDecoration: "underline"
+                                    }
+                                }}
                                 onClick={() => handleOpen("create_info")}
                             >
                                 お知らせを追加する
                             </Button>
-                        }
+                        )}
                     </Typography>
                     <Infos
-                        is_admin={ props.is_admin }
+                        is_admin={props.is_admin}
                         dates={dates}
                         infos={infos}
                         setDates={setDates}
