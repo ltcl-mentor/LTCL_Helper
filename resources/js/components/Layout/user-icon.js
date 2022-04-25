ea07fbe6957ec14686a2464e017bb8d44c47e3f1
Ltcl-mentor <riho.pinehill@gmail.com>
2022-04-25 13:43:40 +0900

    ヘッダーレスポンシブ修正

--- resources/js/components/Layout/user-icon.js
+++ resources/js/components/Layout/user-icon.js
@@ -1,10 +1,16 @@
 import React, {useState} from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import Button from '@mui/material/Button';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import Typography from '@material-ui/core/Typography';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SearchIcon from '@mui/icons-material/Search';
+import AddIcon from '@mui/icons-material/Add';
+import HistoryIcon from '@mui/icons-material/History';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const style = {
     color: '#771AF8'
@@ -25,24 +31,60 @@ const Icon = (props) => {
         setAnchorEl(null);
     };
     
+    // マイページ
     const mypage = () => {
         setAnchorEl(null);
         history.push('/my_page');
     };
     
+    // 管理者のマイページ
     const adminMypage = () => {
         setAnchorEl(null);
         history.push('/Admin_my_page');
     };
     
+    // 質問閲覧履歴
     const historyIndex = () => {
         setAnchorEl(null);
         history.push('/history');
     };
     
+    // 検索画面
+    const toSearch = () => {
+        setAnchorEl(null);
+        history.push('/?page=qa');
+    };
+    
+    const toQuestion = () => {
+        setAnchorEl(null);
+        history.push('/public/questions/create');
+    };
+    
+    // ログアウト
     const logout = () => {
         document.getElementById('logout-form').submit();
     };
+    
+    let responsive;
+    if (!props.isWide) {
+        responsive = (
+            <React.Fragment>
+                <MenuItem onClick={() => toSearch()}>
+                    <ListItemIcon>
+                        <SearchIcon />
+                    </ListItemIcon>
+                    検索する
+                </MenuItem>
+            
+                <MenuItem onClick={() => toQuestion()}>
+                    <ListItemIcon>
+                        <AddIcon />
+                    </ListItemIcon>
+                    質問する
+                </MenuItem>
+            </React.Fragment>
+        );
+    }
     
     return (
         <div>
@@ -79,28 +121,49 @@ const Icon = (props) => {
                 open={ Boolean(anchorEl) }
                 onClose={ handleClose }
             >
-                {props.user.is_admin ? (
-                    <div><MenuItem onClick={() => adminMypage()}>
-                    マイページ
-                    </MenuItem>
+                {props.user.is_admin ? 
+                    <div>
+                        <MenuItem onClick={() => adminMypage()}>
+                            <ListItemIcon>
+                                <AccountCircleIcon />
+                            </ListItemIcon>
+                            マイページ
+                        </MenuItem>
+                        
+                        {responsive}
                     
-                    <MenuItem onClick={handleClose}>
-                        <a style={style} onClick={ logout }>ログアウト</a>
-                    </MenuItem></div>
-                ) :
-                (
-                    <div><MenuItem onClick={() => mypage()}>
-                    マイページ
-                    </MenuItem>
+                        <MenuItem onClick={handleClose}>
+                            <ListItemIcon>
+                                <LogoutIcon />
+                            </ListItemIcon>
+                            <a style={style} onClick={ logout }>ログアウト</a>
+                        </MenuItem>
+                    </div>
+                :
+                    <div>
+                        <MenuItem onClick={() => mypage()}>
+                            <ListItemIcon>
+                                <AccountCircleIcon />
+                            </ListItemIcon>
+                            マイページ
+                        </MenuItem>
                     
-                    <MenuItem onClick={() => historyIndex()}>
-                        質問閲覧履歴
-                    </MenuItem>
+                        <MenuItem onClick={() => historyIndex()}>
+                            <ListItemIcon>
+                                <HistoryIcon />
+                            </ListItemIcon>
+                            質問閲覧履歴
+                        </MenuItem>
+                        
+                        {responsive}
                     
-                    <MenuItem onClick={handleClose}>
-                        <a style={style} onClick={ logout }>ログアウト</a>
-                    </MenuItem></div>
-                )
+                        <MenuItem onClick={handleClose}>
+                            <ListItemIcon>
+                                <LogoutIcon />
+                            </ListItemIcon>
+                            <a style={style} onClick={ logout }>ログアウト</a>
+                        </MenuItem>
+                    </div>
                 }
             </Menu>
         </div>
