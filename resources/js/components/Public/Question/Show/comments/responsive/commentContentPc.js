101b85d8d6b53ed8dd7ec47eade7de419289f099
sho watanabe <showatanabe0226@gmail.com>
2022-05-18 21:18:22 +0900

    [update]パブリック用コンポーネントのTypographyのsxを修正

--- resources/js/components/Public/Question/Show/comments/responsive/commentContentPc.js
+++ resources/js/components/Public/Question/Show/comments/responsive/commentContentPc.js
@@ -4,6 +4,7 @@ import Typography from "@material-ui/core/Typography";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import { styleComment } from "../../../../../Atom/Typography/TypographyStyle";
 
 const commentContentPc = props => {
     return (
