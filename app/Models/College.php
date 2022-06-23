ee1ef430db4c3dd23f5906ee3bf651d7fd25b831
Riho0511 <riho.pinehill@gmail.com>
2022-06-24 02:46:26 +0900

    モデルをModelsフォルダに移動

--- app/Models/College.php
+++ app/Models/College.php
@@ -15,25 +15,25 @@ class College extends Model
     {
         $now = Carbon::now();
         $college_datas = [];
-        
+
         $datas = self::getCollegeApiData($year, $month);
-        
+
         // 開校・閉館時間の代入
         $college_datas['start'] = $datas['values'][$date][3] ?: null;
         $college_datas['close'] = $datas['values'][$date][4] ?: null;
-        
+
         // 校舎出勤メンターの代入
         $college_datas['staff'] = self::inputStaff("college", 5, $date, $datas);
-        
+
         // オンライン出勤メンターの代入
         $college_datas['online_staff'] = self::inputStaff("online", 7, $date, $datas);
-        
+
         // オンライン校舎の代入
         if ($datas['values'][$date][9] == "なし") {
             $college_datas['zoom']['message'] = "本日オンライン質問部屋はありません。\n質問のある方はSlackにて出勤メンターへ連絡してください。";
         } elseif ($datas['values'][$date][9] == "あり") {
             $college_datas['zoom']['message'] = $datas['values'][$date][10];
-            
+
             // オンライン校舎の開校時間中かどうか
             $times = explode("、", $college_datas['zoom']['message']);
             if ($times[0] == "") {
@@ -48,7 +48,7 @@ class College extends Model
             foreach ($openTimes as $openTime) {
                 $startDiff = strtotime($now) - strtotime($openTime[0]);
                 $endDiff = strtotime($now) - strtotime($openTime[1]);
-                
+
                 if (!(($startDiff >= 0 && $endDiff >= 0) || ($startDiff <= 0 && $endDiff <= 0))) {
                     $college_datas['zoom']['ontime'] = true;
                     break;
@@ -56,7 +56,7 @@ class College extends Model
                 $college_datas['zoom']['ontime'] = false;
             }
         }
-        
+
         // オンライン校舎のzoomリンクを掲載するかどうか
         // 当日の開校時間中しか見えないようにする
         if ($now->year == $year && $now->month == $month && $now->day == $date) {
@@ -70,10 +70,10 @@ class College extends Model
             $college_datas['zoom']['exist'] = false;
             $college_datas['zoom']['message'] = "zoomリンクは当日しか\n見れません。";
         }
-        
+
         return $college_datas;
     }
-    
+
     /**
      * 引数に渡された年と月の情報から該当するスプレットシートの全データを取得
      */
@@ -81,16 +81,16 @@ class College extends Model
     {
         $client = new \GuzzleHttp\Client();
         $url = 'https://sheets.googleapis.com/v4/spreadsheets/'. env('GoogleSheetsID') .'/values/'. $year .'_'. $month;
-        
+
         $response = $client->request(
             'GET',
             $url,
             ['query' => ['key' => env('GoogleSheetsKey'), 'majorDimension' => 'COLUMNS']]
         );
-        
+
         return json_decode($response->getBody(), true);
     }
-    
+
     /**
      * 出勤メンター通知
      * 毎日12時にslackへ通知
@@ -99,7 +99,7 @@ class College extends Model
     {
         $date = Carbon::today();
         $message = "";
-        
+
         // 出勤メンター(校舎)
         if (Self::getCollegeData($date->year, $date->month, $date->day)["staff"][0]) {
             $message .= "*■本日の出勤メンター（校舎）*\n";
@@ -108,7 +108,7 @@ class College extends Model
                 $message .= $staff . "\n";
             }
         }
-        
+
         // 出勤メンター(オンライン)
         if (Self::getCollegeData($date->year, $date->month, $date->day)["online_staff"][0]) {
             $message .= "\n*■本日の出勤メンター（オンライン）*\n";
@@ -117,7 +117,7 @@ class College extends Model
                 $message .= $staff . "\n";
             }
         }
-        
+
         // オンライン校舎
         $message .= "\n*■本日のオンライン校舎（質問部屋）*\n";
         $online_time = Self::getCollegeData($date->year, $date->month, $date->day)["zoom"];
@@ -129,16 +129,16 @@ class College extends Model
 
         Slack::sendMessage($message, 'attendance');
     }
-    
+
     /**
      * メンター、出勤時間代入
      **/
     public static function inputStaff($status, $num, $date, $datas) {
         $array = [];
-        
+
         if($datas['values'][$date][$num] !== ""){
             $staffs = explode("\n", $datas['values'][$date][$num], -1);
-            
+
             // メンターと出勤時間を出力
             $i=0;
             foreach($staffs as $staff) {
@@ -148,15 +148,15 @@ class College extends Model
                         break;
                     }
                 }
-                
+
                 // オンライン出勤の場合、時間先頭のアスタリスク削除
                 if ($status == "online" && substr($times, 0, 1) == "*") {
                     $times = substr($times, 1);
                 }
-                
+
                 //「〜」を全て半角に統一
                 $times = str_replace("〜", "~", $times);
-                
+
                 $array[$i] = $i+1 . ". " . explode("　", $staff)[0] . " (" . $times . ")";
                 $i++;
             }
