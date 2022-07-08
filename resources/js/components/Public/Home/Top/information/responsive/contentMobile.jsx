44d3a54f0548a8bae36981239c56d61375572663
Riho0511 <riho.pinehill@gmail.com>
2022-07-08 09:27:12 +0900

    お知らせ部分にwidthを指定

--- resources/js/components/Public/Home/Top/information/responsive/contentMobile.jsx
+++ resources/js/components/Public/Home/Top/information/responsive/contentMobile.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Weather from "../weather";
-import { Heading } from "@/Styles/Public/Home/Top/Information/content";
+import { Heading, GridContent } from "@/Styles/Public/Home/Top/Information/content";
 
 /**
  * モバイル版お知らせ
  */
 const contentMobile = ({ list }) => {
     return (
-        <React.Fragment>
+        <GridContent>
             {list}
             <Heading>天気情報</Heading>
             <Weather isWide={false} />
-        </React.Fragment>
+        </GridContent>
     );
 };
 
